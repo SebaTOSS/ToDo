5f125a449e243d940adbb4965d358d0b6d27fb25
Sebastián Perez Escribano <sebastianescribano@juicerpricing.com>
2024-10-19 17:56:51 -0300

    Added logged support

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
-  });
+  const app = await NestFactory.create(AppModule);
   
   const configService = app.get(ConfigService);
+  const logLevel = configService.get<string>('LOG_LEVEL') || 'combined';
+  app.use(morgan(logLevel));
+
   await app.listen(configService.get<number>('PORT') || 3000);
 }
 
