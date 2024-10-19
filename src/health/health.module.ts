dcddc19cda1bf9ff11f698a660434d0ae1aab853
Sebastián Perez Escribano <sebastianescribano@juicerpricing.com>
2024-10-19 17:49:50 -0300

    Removed unnecesary imports

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthService } from './health.service';
 import { HealthController } from './health.controller';
@@ -10,20 +8,6 @@ import { HealthController } from './health.controller';
   imports: [
     HttpModule,
     TerminusModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
   ],
   controllers: [HealthController],
   providers: [HealthService],
