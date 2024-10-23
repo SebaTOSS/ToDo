739f9a59a9b049a376385d3526968aa7eccc2b46
Sebastián Perez Escribano <sebastianescribano@juicerpricing.com>
2024-10-23 16:28:17 -0300

    Global error handler added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './core';
 import * as morgan from 'morgan';
 
 async function bootstrap() {
@@ -11,7 +12,13 @@ async function bootstrap() {
   const logLevel = configService.get<string>('LOG_LEVEL') || 'combined';
   app.use(morgan(logLevel));
 
-  app.useGlobalPipes(new ValidationPipe());
+  const validationPipe = new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  });
+  app.useGlobalPipes(validationPipe);
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(configService.get<number>('PORT') || 3000);
 }
