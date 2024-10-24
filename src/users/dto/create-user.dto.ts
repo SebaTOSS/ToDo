1b5186cf3d71d4e4416316db96b2c1d4b2f72056
Sebastián Perez Escribano <sebastianescribano@juicerpricing.com>
2024-10-24 18:13:47 -0300

    Added swagger for document the API

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,18 @@
-import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+    @ApiProperty({ example: 'John Doe' })
     @IsString()
     readonly name: string;
-
+    
+    @ApiProperty({ example: 'password123' })
     @IsString()
+    @MinLength(6)
     readonly email: string;
-
+    
+    @ApiProperty({ example: 'user@example.com' })
     @IsString()
+    @IsEmail()
     readonly password: string;
 }
